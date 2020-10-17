1fd8b1b6cf7491e3ea024b5e6082ccfd8ceb318b
shahid66 <kk.shahid66@gmail.com>
2020-10-18 01:59:03 +0600

    heroku

--- index.js
+++ index.js
@@ -54,6 +54,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT||port)
