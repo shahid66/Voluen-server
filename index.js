2bdc96ef0eef5529e35f05b69f1b66a0d9904ebe
shahid66 <kk.shahid66@gmail.com>
2020-10-18 11:02:37 +0600

    k

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxt7l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db(DB_NAME).collection("Events");
-  const registerEvent = client.db(DB_NAME).collection("registerEvent");
+  const collection = client.db(process.env.DB_NAME).collection("Events");
+  const registerEvent = client.db(process.env.DB_NAME).collection("registerEvent");
 
   // app.post('/addEvent',(req,res)=>{
   //     const events=req.body;
